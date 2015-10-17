abccd10457f95db6b618abaf7c0810781d19ff76
Mark J. Reed <markjreed@gmail.com>
2015-10-16 22:29:04 -0400

    Step 3½, missing from http://filterrific.clearcove.ca: add arrays for dropdowns to controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,8 +6,17 @@ class PlayersController < ApplicationController
   # GET /players.json
   def index
     @filterrific = 
-      initialize_filterrific(Player, 
-                             params[:filterrific]) or return
+      initialize_filterrific(
+        Player, 
+        params[:filterrific],
+        select_options: {
+          sorted_by: %w{name_asc name_desc 
+                        server_name_asc server_name_desc
+                        class_name_asc class_name_desc
+                        spec_name_asc spec_name_desc},
+          with_class_name: Player.all.map(&:class_name).uniq
+        }
+    ) or return
     @players = @filterrific.find
   end
 
