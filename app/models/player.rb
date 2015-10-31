8f4a7d420f1e87ea6c543e6cad60bceffc8e247a
Mark J. Reed <markjreed@gmail.com>
2015-10-31 17:23:32 -0400

    Add teammate filter

--- app/models/player.rb
+++ app/models/player.rb
@@ -9,10 +9,11 @@ class Player < ActiveRecord::Base
     available_filters: [
       :sorted_by,
       :with_class_name,
-      :with_spec_name
+      :with_spec_name,
+      :with_teammate
     ]
   )
-  
+
   scope :sorted_by, lambda { |sort_key|
     direction = sort_key =~ /desc$/ ? 'desc' : 'asc'
     case sort_key.to_s
@@ -22,21 +23,27 @@ class Player < ActiveRecord::Base
       raise ArgumentError, "Invalid sort option: #{ sort_option.inspect }"
     end
   }
-  
+
   scope :with_class_name, lambda { |class_names|
     where class_name: [*class_names]
   }
-  
+
   scope :with_spec_name, lambda { |spec_names|
     where spec_name: [*spec_names]
   }
-  
+
   scope :with_match, lambda { |matches|
     where match: [*matches]
   }
- 
+
+  scope :with_teammate, lambda { |players|
+    where id: [*players].inject([]) { |a,p| 
+       Player.find(p).scores.inject(a) {|a,s| a += Score.where(match: s.match, player_faction: s.player_faction).map{|s|s.player} }
+    }.uniq.map(&:id)
+  }
+
   def self.options_for_select
     order('LOWER(name)').map { |e| [e.name, e.id] }
   end
-  
+
 end
